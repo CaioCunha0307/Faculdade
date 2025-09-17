edce14565fafd3acdf62ad6c4fe32f078c87e886
CaioCunha0307 <caio.ocunha@sempreceub.com>
2025-09-17 10:43:02 -0300

    Update ExercicioPython01.py

--- ExercicioPython01.py
+++ ExercicioPython01.py
@@ -1,7 +1,4 @@
-# Lê o valor do salário como um número de ponto flutuante
 salario = float(input())
-
-# Verificar as faixas de imposto e calcula o valor devido
 if salario <= 2000.00:
     print("Isento")
 elif salario <= 3000.00:
@@ -10,6 +7,6 @@ elif salario <= 3000.00:
 elif salario <= 4500.00:
     imposto = (1000.00 * 0.08) + (salario - 3000.00) * 0.18
     print(f"R$ {imposto:.2f}")
-else: # Para salários acima de 4500.00
+else:
     imposto = (1000.00 * 0.08) + (1500.00 * 0.18) + (salario - 4500.00) * 0.28
     print(f"R$ {imposto:.2f}")
